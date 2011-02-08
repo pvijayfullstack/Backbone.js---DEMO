1f3489acba423778908c0c38a959286bea44b5cc
xTrol <xtrol87@gmail.com>
2011-02-08 21:41:53 +0100

    sass confing

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Sass::Plugin.options[:template_location] = "#{Rails.root}/app/sass"
+
 # Initialize the rails application
 BackboneDemo::Application.initialize!
